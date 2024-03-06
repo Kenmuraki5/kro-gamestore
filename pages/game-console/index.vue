65bd7322bdbcfa647511f1845ea81626c76a901d
kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2024-03-07 02:46:17 +0700

    fix bug

--- pages/game-console/index.vue
+++ pages/game-console/index.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="container mx-auto">
     <h1 class="text-3xl font-bold m-4">Products</h1>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <div v-if="gameConsoleStore.gameConsole" v-for="product in gameConsoleStore.gameConsole" class="bg-white p-4 m-10 rounded-lg shadow-md">
-        <cardProduct :product="product"></cardProduct>
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+    >
+      <div v-if="gameConsoleStore.gameConsole">
+        <div
+          v-for="product in gameConsoleStore.gameConsole"
+          :key="product.Id"
+          class="bg-white p-4 m-10 rounded-lg shadow-md"
+        >
+          <cardProduct :product="product"></cardProduct>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, watch } from 'vue';
-import { useGameConsoleStore } from '@/store/console';
+import { ref, onMounted, watch } from "vue";
+import { useGameConsoleStore } from "@/store/console";
 
 const gameConsoleStore = useGameConsoleStore();
 </script>
