fbb7e9c66310d9ffaf5578847a19a5110a66374d
kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2024-03-09 22:47:49 +0700

    fix jenkins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     public: {
-      baseURL: process.env.BASE_URL || 'http://localhost:8080/',
+      baseURL: process.env.BASE_URL || 'http://localhost/api',
     },
-  }
+  },
 })
